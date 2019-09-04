be23afd0266b3da850226ea0c4b8368016123611
Horst Heistermann <horst.heistermann@gmail.com>
2019-09-03 20:29:12 -0700

    Horst's react test

---
+++ src/build/precache-manifest.5561acc695092c3fbc362ba8c0d5194a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3d04cce56acfdd6d49c2",
+    "url": "/static/js/main.7185dddd.chunk.js"
+  },
+  {
+    "revision": "8b62809101a1cd616a9c",
+    "url": "/static/js/2.58810e3d.chunk.js"
+  },
+  {
+    "revision": "3d04cce56acfdd6d49c2",
+    "url": "/static/css/main.b9f9e5f7.chunk.css"
+  },
+  {
+    "revision": "db6c4ddd34cbc18f99919e64f97965ac",
+    "url": "/index.html"
+  }
+];
